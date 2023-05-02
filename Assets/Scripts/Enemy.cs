509abcf41644b5aa61543216f3823d4719e013ef
JheysonGalvis <tecnojheyson@gmail.com>
2023-05-02 15:21:38 -0500

    hasta el video 17 completo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Transform player;
+    [SerializeField] int health = 1;
+    [SerializeField] float speed = 1;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+    
+    private void Update () 
+    {
+        Vector2 direction = player.position - transform.position;
+        transform.position += (Vector3)direction.normalized * Time.deltaTime * speed;
+        //transform.position += (Vector3)direction * Time.deltaTime * speed;
+    }
+
+    public void TakeDamage(){
+        health--;
+        if(health <=0){
+            Destroy(gameObject);
+        }
+    }
+    
+    private void OnTriggerEnter2D (Collider2D collision) 
+    {
+        if(collision.CompareTag("Player")){
+            collision.GetComponent<Player>().TakeDamage();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float h;
+    float v;
+    Vector3 moveDirection;
+    [SerializeField]float speed = 5;
+    [SerializeField]Transform aim;
+    [SerializeField]Camera camera;
+    Vector2 facingDirection;
+    [SerializeField]Transform bulletPrefab;
+    bool gunLoaded = true;
+    [SerializeField]float fireRate = 1;
+    [SerializeField]int health = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        moveDirection.x = h;
+        moveDirection.y = v;
+
+        transform.position += moveDirection * Time.deltaTime * speed;
+
+        //Movimiento de la mira
+        facingDirection = camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        aim.position = transform.position + (Vector3)facingDirection.normalized;
+
+        if(Input.GetMouseButton(0) && gunLoaded) { 
+            gunLoaded = false;
+            float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            Instantiate(bulletPrefab, transform.position, targetRotation);
+            StartCoroutine(ReloadGun());
+        }
+    }
+
+    IEnumerator ReloadGun(){
+        yield return new WaitForSeconds(1/fireRate);
+        gunLoaded = true;
+    }
+    public void TakeDamage () {
+        health--;
+        if(health <=0)
+        {
+            //Game over;
+        }
+    }
+}
